676816b97ee1a5fd56bd0cf0760af36f8002b8f7
erik-scholtz <erikscholtz23@gmail.com>
2026-03-10 17:35:22 -0400

    hlir validation init

--- src/hlir/util/mod.rs
+++ src/hlir/util/mod.rs
@@ -2,3 +2,4 @@ pub mod assign_func;
 pub mod assign_vars;
 pub mod style_pass;
 pub mod style_resolver;
+pub mod validation_pass;
---
+++ src/hlir/util/validation_pass.rs
@@ -0,0 +1,17 @@
+use std::collections::HashMap;
+
+use crate::hlir::HLIRModule;
+use crate::hlir::ir_types::{Func, FuncBlock, Global, Id, Op};
+
+impl HLIRModule {
+    pub fn validate(&self, hlir: &HLIRModule) -> Result<(), String> {
+        self.validate_globals(&hlir.globals);
+        self.validate_functions(&hlir.functions);
+
+        Ok(())
+    }
+
+    fn validate_globals(&self, globals: &HashMap<Id, Global>) {}
+
+    fn validate_functions(&self, functions: &HashMap<Id, Func>) {}
+}
