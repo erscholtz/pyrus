4d95434e68ab419ffa30bd876f3bd52380adbabc
erscholtz <erikscholtz23@gmail.com>
2026-08-17 17:16:05 -0400

    Test invalid element invocation trivia

--- tests/lexer/diagnostics.rs
+++ tests/lexer/diagnostics.rs
@@ -1,5 +1,5 @@
 use pyrus::diagnostic::{CompilerDiagnostic, SyntaxError};
-use pyrus::lexer::{self, tokens::TokenKind};
+use pyrus::lexer;
 
 #[test]
 fn records_unknown_character_as_syntax_diagnostic() {
@@ -10,4 +10,19 @@ fn records_unknown_character_as_syntax_diagnostic() {
         Some(CompilerDiagnostic::Syntax(SyntaxError::InvalidConstruct { construct, .. }))
             if construct == "character"
     ));
+}
+
+#[test]
+fn rejects_comment_between_at_and_element_name() {
+    let result =
+        lexer::lex_all("@ // comment\ntext[Hello]", "comment_after_at");
+
+    assert!(result.is_err());
+}
+
+#[test]
+fn rejects_newline_between_at_and_element_name() {
+    let result = lexer::lex_all("@\ntext[Hello]", "newline_after_at");
+
+    assert!(result.is_err());
 }
